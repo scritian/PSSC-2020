c01bce11914a3493eb570b89f2acf6c5e6324de2
scritian <scritian@yahoo.com>
2020-11-21 17:54:34 +0200

    Start project

--- Sandor-Cristian/Project/Samples/StackUnderflow.API.Rest/Controllers/BackofficeController.cs
+++ Sandor-Cristian/Project/Samples/StackUnderflow.API.Rest/Controllers/BackofficeController.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Access.Primitives.Extensions.ObjectExtensions;
 using Access.Primitives.IO;
 using Microsoft.AspNetCore.Mvc;
-using StackUnderflow.Backend.Interfaces.Responses;
 using StackUnderflow.Domain.Core;
 using StackUnderflow.Domain.Core.Contexts;
 using StackUnderflow.Domain.Schema.Backoffice.CreateTenantOp;
 using StackUnderflow.EF.Models;
 using Access.Primitives.EFCore;
+using StackUnderflow.Domain.Schema.Backoffice.InviteTenantAdminOp;
+using StackUnderflow.Domain.Schema.Backoffice;
+using LanguageExt;
 
 namespace StackUnderflow.API.Rest.Controllers
 {
@@ -27,44 +29,26 @@ namespace StackUnderflow.API.Rest.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpPost("createTenant")]
+        [HttpPost("tenant")]
         public async Task<IActionResult> CreateTenantAsyncAndInviteAdmin([FromBody] CreateTenantCmd createTenantCmd)
         {
-            var ctx = await _dbContext.LoadAsync("dbo.BackofficeHttpController", new
-            {
-                OrganisationId = createTenantCmd.OrganisationId
-            }, async reader =>
-            {
-                var tenants = await reader.ReadAsync<Tenant>();
-                var tenantUsers = await reader.ReadAsync<TenantUser>();
-                var users = await reader.ReadAsync<User>();
+            BackofficeWriteContext ctx = new BackofficeWriteContext(new List<Tenant>(), new List<TenantUser>(), new List<User>());
+            var dependencies = new BackofficeDependencies();
+            dependencies.GenerateInvitationToken = () => Guid.NewGuid().ToString();
+            dependencies.SendInvitationEmail = (InvitationLetter letter) => async ()=>new InvitationAcknowledgement(Guid.NewGuid().ToString());
 
-                _dbContext.AttachRange(tenants);
-                _dbContext.AttachRange(tenantUsers);
-                _dbContext.AttachRange(users);
-                return new BackofficeWriteContext(
-                    new EFList<Tenant>(_dbContext.Tenant),
-                    new EFList<TenantUser>(_dbContext.TenantUser),
-                    new EFList<User>(_dbContext.User));
-            });
+            var expr = from createTenantResult in BackofficeDomain.CreateTenant(createTenantCmd)
+                       let adminUser = createTenantResult.SafeCast<CreateTenantResult.TenantCreated>().Select(p => p.AdminUser)
+                       let inviteAdminCmd = new InviteTenantAdminCmd(adminUser)
+                       from inviteAdminResult in BackofficeDomain.InviteTenantAdmin(inviteAdminCmd)
+                       select new { createTenantResult, inviteAdminResult };
 
-            var expr = from createResult in BackofficeDomain.CreateTenant(createTenantCmd.OrganisationId,
-                                createTenantCmd.TenantName, createTenantCmd.Description, createTenantCmd.AdminEmail, createTenantCmd.AdminName, createTenantCmd.UserId)
-                       let adminUser = createResult.SafeCast<CreateTenantResult.TenantCreated>().Select(p => p.User)
-                       from u in BackofficeDomain.InviteTenantAdmin(adminUser)
-                       select new { createResult, u };
+            var r = await _interpreter.Interpret(expr, ctx, dependencies);
 
-            var r = await _interpreter.Interpret(expr, ctx);
-
-            await _dbContext.SaveChangesAsync();
-
-            return r.createResult.Match(
-                created => (IActionResult)Ok(new CreateTenantAndAdminResponse(true, created.Tenant.TenantId, created.User.DisplayName)),
-                notCreated => BadRequest(new CreateTenantAndAdminResponse(false, 0, string.Empty)),
-                invalidRequest => BadRequest(new CreateTenantAndAdminResponse(false, 0, string.Empty)));
+            return r.createTenantResult.Match(
+                created => (IActionResult)Ok(created.Tenant.TenantId),
+                notCreated => BadRequest("Tenant could not be created."),
+                invalidRequest => BadRequest("Invalid request."));
         }
-
-
-
     }
 }
